21356026c161f0c90cf1f0374ea895f64a253550
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-26 08:10:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -88,11 +88,6 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
           </span>
         </a>
       </footer>
